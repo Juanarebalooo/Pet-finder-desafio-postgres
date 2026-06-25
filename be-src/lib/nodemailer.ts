0059f1282ab596b1b764e323a6a12e25ea40d820
Juanarebalooo <juanarebalooo@gmail.com>
2026-06-25 19:21:10 -0300

    sixth commit

--- be-src/lib/nodemailer.ts
+++ be-src/lib/nodemailer.ts
@@ -3,7 +3,9 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
